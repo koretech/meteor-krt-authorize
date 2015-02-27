109cb72f9519d2bcee68e033e2ecfc6484054f58
Mike Kornelson <mike@koretech.ca>
2015-02-27 16:10:17 -0600

    rewrote most of the authorization library

--- package.js
+++ package.js
@@ -1,8 +1,9 @@
 Package.describe({
 	name: 'krt:authorize',
 	summary: 'Koretech Authorize Package',
-	version: '0.1.1',
-	git: 'https://github.com/koretech/meteor-krt-authorize.git'
+	version: '0.2.0',
+	git: 'https://github.com/koretech/meteor-krt-authorize.git',
+	documentation: null
 });
 
 Package.onUse(function(api){
@@ -12,19 +13,30 @@ Package.onUse(function(api){
 	api.use([
 		'underscore',
 		'accounts-base',
-		'krt:core@0.1.0',
+		'tracker',
+		'check',
+		'templating',
+		'krt:core@0.1.2',
 		'alanning:roles@1.2.13'
 	], ['client', 'server']);
 
 	api.imply([
 		'accounts-base',
 		'krt:core',
-		'alanning:roles'
+		'alanning:roles@1.2.13'
 	]);
 
 	api.addFiles([
 		'namespaces.js',
-		'util.js'
+		'lib/authorize_common.js'
 	], ['client', 'server']);
+
+	api.addFiles([
+		'lib/authorize_client.js'
+	], 'client');
+
+	api.addFiles([
+		'lib/authorize_server.js'
+	], 'server');
 
 });
